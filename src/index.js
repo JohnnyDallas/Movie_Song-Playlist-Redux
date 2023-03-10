31d439819f9982ea4406fb155e23e181aa198715
Johnny <vfscorbandallas@gmail.com>
2023-03-10 10:37:28 +0700

    First commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import "bulma/css/bulma.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import App from "./App";
+import { store } from './store';
+
+const rootElement = document.getElementById("root");
+const root = ReactDOM.createRoot(rootElement);;
+
+root.render(
+ <Provider store={store}>  
+   <App />
+ </Provider>  
+);
